e7cd51d1ad6c7d76eda42ee825539f9e3df491d0
Yoongs <sys848367@gmail.com>
2025-07-04 13:27:56 +0900

    create branch

---
+++ DailyCS/DailyCS/Views/SavedQuestionView.swift
@@ -0,0 +1,18 @@
+//
+//  SavedQuestionView.swift
+//  DailyCS
+//
+//  Created by 송서윤 on 7/4/25.
+//
+
+import SwiftUI
+
+struct SavedQuestionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SavedQuestionView()
+}
