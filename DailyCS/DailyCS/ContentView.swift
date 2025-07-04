603de4e047663b2d4801c628147db49efbc55389
rrheon <yongheon0806@gmail.com>
2025-07-04 11:48:17 +0900

    Git을 사용합시다

---
+++ DailyCS/DailyCS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyCS
+//
+//  Created by 최용헌 on 7/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyCS/DailyCS/DailyCSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyCSApp.swift
+//  DailyCS
+//
+//  Created by 최용헌 on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyCSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
