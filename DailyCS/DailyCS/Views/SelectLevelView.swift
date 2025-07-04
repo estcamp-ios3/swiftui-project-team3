d0cc2f999e0ff97e391bcf707de0281e7a4c9241
강준환 <etv7796@gmail.com>
2025-07-04 13:55:51 +0900

    Update SelectLevelView.swift

--- DailyCS/DailyCS/Views/SelectLevelView.swift
+++ DailyCS/DailyCS/Views/SelectLevelView.swift
@@ -8,16 +8,60 @@
 import SwiftUI
 
 struct SelectLevelView: View {
+    
+    @State var difficultyLevel: Int = 1
+        
     var body: some View {
         
-        Button(action: {}) {
-            Text("쉬움")
+        Spacer()
+        
+        Text("난이도 선택").font(.largeTitle).bold(true)
+            .padding(.bottom, 50)
+        
+        Button("쉬움") {
+            difficultyLevel = 1
         }
+        .padding()
+        .font(.title)
+        
+        Button("보통") {
+            difficultyLevel = 2
+        }
+        .padding()
+        .font(.title)
+        
+        Button("어려움") {
+            difficultyLevel = 3
+        }
+        .padding()
+        .font(.title)
+        
+        Spacer()
+        
+        Text("현재 난이도 : " + printLevel())
+        
+        Spacer()
         
         NavigationLink(destination: CSQuestionView()) {
-            Text("시작")
+            Text("시작").font(.title)
         }
         
+        Spacer()
+        
+    }
+    
+    func printLevel() -> String {
+        switch difficultyLevel {
+        case 1:
+            "쉬움"
+        case 2:
+            "보통"
+        case 3:
+            "어려움"
+        default:
+            "난이도를 선택해주세요."
+        }
+   
     }
 }
 
