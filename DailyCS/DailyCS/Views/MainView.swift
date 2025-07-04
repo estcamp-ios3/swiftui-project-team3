116fec4bfd0c41ec3c6ea98466b307ab0562d653
강준환 <etv7796@gmail.com>
2025-07-04 13:05:04 +0900

    add mainview

---
+++ DailyCS/DailyCS/Views/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  DailyCS
+//
+//  Created by home on 7/4/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("하루 CS")
+            .font(.largeTitle)
+            .bold(true)
+    }
+}
+
+#Preview {
+    MainView()
+}
