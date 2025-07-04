026a9f8434375e7de7159d4bdf1f42a558eaa450
Hye-seong77 <rurushu77@naver.com>
2025-07-04 15:09:30 +0900

    질문 결과 뷰

---
+++ DailyCS/DailyCS/Views/QuestionResultView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionResultView.swift
+//  DailyCS
+//
+//  Created by Rental pc on 7/4/25.
+//
+
+import SwiftUI
+
+struct QuestionResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionResultView()
+}
---
+++ DailyCS/DailyCS/Views/UserInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  UserInfoView.swift
+//  DailyCS
+//
+//  Created by Rental pc on 7/4/25.
+//
+
+import SwiftUI
+
+struct UserInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserInfoView()
+}
