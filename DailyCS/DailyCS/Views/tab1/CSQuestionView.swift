f0714f687872c63bd5def322412994bfa5ef81d6
강준환 <etv7796@gmail.com>
2025-07-07 13:52:20 +0900

    Update CSQuestionView.swift

--- DailyCS/DailyCS/Views/tab1/CSQuestionView.swift
+++ DailyCS/DailyCS/Views/tab1/CSQuestionView.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 
 
+
+
 struct CSQuestionView: View {
-  
-  var difficultyLevel: Int = 1
-  
+    
+    @State var yourAnswer: Int = 0
+    @State var ChosenQuestion: Int = 0
     var questionData = questionDummyDatas.shuffled().prefix(5)
+    var difficultyLevel: Int = 1
+    
     
   var body: some View {
     VStack {
@@ -23,16 +27,37 @@ struct CSQuestionView: View {
           .font(.headline)
         
         List {
-            Text("1. " + questionDummyDatas[0].answers(index: 1))
-            Text("2. " + questionDummyDatas[0].answers(index: 2))
-            Text("3. " + questionDummyDatas[0].answers(index: 3))
-            Text("4. " + questionDummyDatas[0].answers(index: 4))
+            
+            Button(action: {yourAnswer = 1}
+                ) {
+                Text("1. " + questionDummyDatas[ChosenQuestion].answers(index: 1))
+            }
+            
+            Button(action: {yourAnswer = 2}
+            ) {
+                Text("2. " + questionDummyDatas[ChosenQuestion].answers(index: 2))
+            }
+            
+            Button(action: {yourAnswer = 3}
+            ) {
+                Text("3. " + questionDummyDatas[ChosenQuestion].answers(index: 3))
+            }
+           
+            Button(action: {yourAnswer = 4})
+            {
+                Text("4. " + questionDummyDatas[ChosenQuestion].answers(index: 4))
+            }
         }
+          if yourAnswer != 0 {
+          Text("Your answer: \(yourAnswer). \(questionDummyDatas[ChosenQuestion].answers(index: yourAnswer))")
+          }
     }
         
       }.padding(30)
       
-      NavigationLink(destination:CSQuestionView()) {
+      Button(action: {
+          
+      }) {
         Text("다음 문제")
           .frame(width: 200, height: 50)
           .background(Color.mainGreen)
