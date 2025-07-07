a1e65f1dbce90508a2bfac9a88c482b0e66ae8f8
강준환 <etv7796@gmail.com>
2025-07-07 13:58:06 +0900

    Update CSQuestionView.swift

--- DailyCS/DailyCS/Views/tab1/CSQuestionView.swift
+++ DailyCS/DailyCS/Views/tab1/CSQuestionView.swift
@@ -14,7 +14,7 @@ struct CSQuestionView: View {
     
     @State var yourAnswer: Int = 0
     @State var ChosenQuestion: Int = 0
-    var questionData = questionDummyDatas.shuffled().prefix(5)
+    var questionDatas = questionDummyDatas.shuffled().prefix(5)
     var difficultyLevel: Int = 1
     
     
@@ -23,46 +23,53 @@ struct CSQuestionView: View {
       
       VStack(alignment: .leading) {
         
-        Text(questionDummyDatas[0].question)
+        Text(questionDatas[ChosenQuestion].question)
           .font(.headline)
         
         List {
             
             Button(action: {yourAnswer = 1}
                 ) {
-                Text("1. " + questionDummyDatas[ChosenQuestion].answers(index: 1))
+                Text("1. " + questionDatas[ChosenQuestion].answers(index: 1))
             }
             
             Button(action: {yourAnswer = 2}
             ) {
-                Text("2. " + questionDummyDatas[ChosenQuestion].answers(index: 2))
+                Text("2. " + questionDatas[ChosenQuestion].answers(index: 2))
             }
             
             Button(action: {yourAnswer = 3}
             ) {
-                Text("3. " + questionDummyDatas[ChosenQuestion].answers(index: 3))
+                Text("3. " + questionDatas[ChosenQuestion].answers(index: 3))
             }
            
             Button(action: {yourAnswer = 4})
             {
-                Text("4. " + questionDummyDatas[ChosenQuestion].answers(index: 4))
+                Text("4. " + questionDatas[ChosenQuestion].answers(index: 4))
             }
         }
           if yourAnswer != 0 {
-          Text("Your answer: \(yourAnswer). \(questionDummyDatas[ChosenQuestion].answers(index: yourAnswer))")
+          Text("Your answer: \(yourAnswer). \(questionDatas[ChosenQuestion].answers(index: yourAnswer))")
           }
     }
         
       }.padding(30)
-      
-      Button(action: {
-          
-      }) {
-        Text("다음 문제")
-          .frame(width: 200, height: 50)
-          .background(Color.mainGreen)
-          .foregroundColor(.white)
-          .cornerRadius(20)
+      if ChosenQuestion < questionDatas.count - 1 {
+              
+          Button(action: {
+              ChosenQuestion += 1
+          }) {
+            Text("다음 문제")
+              .frame(width: 200, height: 50)
+              .background(Color.mainGreen)
+              .foregroundColor(.white)
+              .cornerRadius(20)
+          }
+      }
+      else{
+          NavigationLink(destination: QuestionResultView()) {
+              Text("다 풀었어요~")
+          }
       }
     }
   }
