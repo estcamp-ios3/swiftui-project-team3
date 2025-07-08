b57923f2b48d3d717a564ee1919c297debbdbf28
rrheon <yongheon0806@gmail.com>
2025-07-08 17:35:33 +0900

    전체 질문리스트 구현 중 - 스크롤 시 마지막 id를 찾으면 데이터 가져오기 - 난이도 버튼 터치 시 해당 난이도의 문제만 가져오기

--- DailyCS/DailyCS/Views/Tab3/EntireQuestionsView.swift
+++ DailyCS/DailyCS/Views/Tab3/EntireQuestionsView.swift
@@ -14,77 +14,161 @@ enum LevelCase {
   case normal
   case hard
   
-  var value: String {
+  
+  /// 버튼 및 네비게이션 타이틀
+  var titleValue: String {
     switch self {
     case .easy: "Easy"
     case .normal: "Normal"
     case .hard: "Hard"
     }
   }
-}
-
-struct EntireQuestionsView: View {
   
-  var body: some View {
-    NavigationStack {
-      VStack(alignment: .leading){
-        Text("난이도를 선택하세요")
-          .padding(.horizontal, 20)
-        
-        Spacer()
-
-        VStack(alignment: .center, spacing: 20) {
-          NavigationLink(destination: LevelBasedQuestionsView(navigtaionTitle: LevelCase.easy.value)) {
-            Text(LevelCase.easy.value)
-              .modifier(LevelButtonStyle(color: .correctGreen))
-          }
-
-          NavigationLink(destination: LevelBasedQuestionsView(navigtaionTitle: LevelCase.normal.value)) {
-            Text(LevelCase.normal.value)
-              .modifier(LevelButtonStyle(color: .correctGreen))
-          }
-
-          NavigationLink(destination: LevelBasedQuestionsView(navigtaionTitle: LevelCase.hard.value)) {
-            Text(LevelCase.hard.value)
-              .modifier(LevelButtonStyle(color: .correctGreen))
-          }
-        }
-        .padding(.horizontal, 16)
-        Spacer()
-      }
-      .modifier(BackgroundStyle(navigationTitle: "전체 문제"))
+  
+  /// 난이도 반환
+  var convertNum: Int {
+    switch self {
+    case .easy: 1
+    case .normal: 2
+    case .hard: 3
     }
   }
 }
 
-
-/// 버튼 스타일
-struct LevelButtonStyle: ViewModifier {
-  let color: Color
-  func body(content: Content) -> some View {
-    content
-      .font(.title)
-      .foregroundColor(.white)
-      .padding()
-      .frame(maxWidth: .infinity)
-      .background(color)
-      .cornerRadius(10)
+struct EntireQuestionsView: View {
+  
+  @StateObject private var csDataManager = CSDataManager.shared
+  
+  @State private var selectedLevel: String = "전체"
+  @State private var startIndex: Int = 1
+  @State private var endIndex: Int = 10
+  
+  
+  private var allLevels: [String] = [
+    LevelCase.easy.titleValue,
+    LevelCase.normal.titleValue,
+    LevelCase.hard.titleValue
+  ]
+  
+  
+  var body: some View {
+    
+    NavigationStack {
+      VStack {
+        ScrollView(.horizontal, showsIndicators: false) {
+          HStack(spacing: 17) {
+            Button("전체") {
+              selectedLevel = "전체"
+              fetchQeustions()
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(selectedLevel == "전체" ? .correctGreen : .gray)
+            .font(.headline)
+            
+            ForEach(allLevels, id: \.self) { level in
+              Button(level){
+                selectedLevel = level
+                fetchQeustions()
+              }
+              .buttonStyle(.borderedProminent)
+              .tint(selectedLevel == level ? .correctGreen : .gray)
+              .font(.headline)
+            }
+          }
+          .padding(.horizontal, 20)
+        }
+        
+        Spacer()
+        
+        List(csDataManager.totalQuestions) { question in
+          NavigationLink {
+            //            SavedQuestionDetailView(question: question)
+            
+            Text("test")
+          } label: {
+            VStack(alignment: .leading, spacing: 10) {
+              Text(selectedLevel)
+              
+              Text(question.question)
+            }
+          }
+          .onAppear {
+            
+            // 리스트 스크롤 시 문제의 아이디가 서버에서 가져온 질문의 마지막 아이디와 같다면 데이터 더 불러오기
+            // 불러오고 시간 텀 두기
+            if question.id == csDataManager.totalQuestions.last?.id {
+              
+              DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                print("마지막")
+                startIndex += 10
+                endIndex += 10
+                csDataManager.fetchCSQuestionWithPaging(level: convertLevel(), from: startIndex, to: endIndex)
+              }
+            }
+          }
+        }
+        .onAppear {
+          // 처음 리스트 시작 시 서버에서 데이터 가져오기
+          csDataManager.fetchCSQuestionWithPaging()
+        }
+        
+        
+      }
+      .modifier(BackgroundStyle(navigationTitle: "전체 문제"))
+    }
+  }
+  
+  
+  ///  convert 레벨  to Int level
+  func convertLevel() -> Int {
+    switch selectedLevel {
+    case LevelCase.easy.titleValue: 1
+    case LevelCase.normal.titleValue: 2
+    case LevelCase.hard.titleValue: 3
+    default:  1
+    }
+  }
+  
+  
+  /// 질문 가져오기
+  func fetchQeustions(){
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+      startIndex = 0
+      endIndex = 10
+      csDataManager.totalQuestions.removeAll()
+      csDataManager.fetchCSQuestionWithPaging(level: convertLevel(), from: startIndex, to: endIndex)
+    }
   }
 }
-
-// 배경 스타일 + 네비게이션타이틀
-struct BackgroundStyle: ViewModifier {
-  let navigationTitle: String
-  func body(content: Content) -> some View {
-    content
-      .frame(maxWidth: .infinity, maxHeight: .infinity)
-      .background(Color.veryLightGreenBackground)
-      .navigationTitle(navigationTitle)
-      .navigationBarTitleDisplayMode(.large)
+  
+  
+  /// 버튼 스타일
+  struct LevelButtonStyle: ViewModifier {
+    let color: Color
+    func body(content: Content) -> some View {
+      content
+        .font(.title)
+        .foregroundColor(.white)
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(color)
+        .cornerRadius(10)
+    }
   }
-}
-
-
-#Preview {
-  EntireQuestionsView()
-}
+  
+  // 배경 스타일 + 네비게이션타이틀
+  struct BackgroundStyle: ViewModifier {
+    let navigationTitle: String
+    func body(content: Content) -> some View {
+      content
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.veryLightGreenBackground)
+        .navigationTitle(navigationTitle)
+        .navigationBarTitleDisplayMode(.large)
+    }
+  }
+  
+  
+  #Preview {
+    EntireQuestionsView()
+  }
